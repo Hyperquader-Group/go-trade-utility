2cfab677383d0cd06b286a3337656df133c7ed65
numbp <o9o73286699@gmail.com>
2019-11-26 20:49:01 +0900

    add vwap ma

--- price/ma.go
+++ price/ma.go
@@ -1,9 +1,8 @@
 package price
 
 import (
-	"fmt"
+	"errors"
 	"sync"
-	"time"
 
 	"github.com/mxmCherry/movavg"
 )
@@ -19,33 +18,52 @@ type Prices struct {
 	Volumes []float64
 }
 
-func SMA(s *Source) float64 {
-	start := time.Now()
-	defer func() {
-		end := time.Now()
-		fmt.Println("exec time: ", end.Sub(start))
-	}()
+func SMA(isVWAP bool, s *Source) (div float64, err error) {
+	if isVWAP {
+		if s.Long.Volumes == nil {
+			return 0, errors.New("vwap volume is nil")
+		} else if s.Short.Volumes == nil {
+			return 0, errors.New("vwap volume is nil")
+		}
+		if len(s.Long.Prices) != len(s.Long.Volumes) {
+			return 0, errors.New("vwap long price & volume length dont match")
+		} else if len(s.Short.Prices) != len(s.Short.Volumes) {
+			return 0, errors.New("vwap short price & volume length dont match")
+		}
+	}
 	maLong := movavg.NewSMA(s.Long.Term)
 	maShort := movavg.NewSMA(s.Short.Term)
 
 	var wg sync.WaitGroup
 	wg.Add(1)
 	go func() {
-		for _, f := range s.Long.Prices {
-			maLong.Add(f)
+		if !isVWAP {
+			for _, f := range s.Long.Prices {
+				maLong.Add(f)
+			}
+		} else { // 出来高加重平均
+			for i, f := range s.Long.Prices {
+				maLong.Add(f * s.Long.Volumes[i])
+			}
 		}
 		wg.Done()
 	}()
 
 	wg.Add(1)
 	go func() {
-		for _, f := range s.Short.Prices {
-			maShort.Add(f)
+		if !isVWAP {
+			for _, f := range s.Short.Prices {
+				maShort.Add(f)
+			}
+		} else { // 出来高加重平均
+			for i, f := range s.Short.Prices {
+				maShort.Add(f * s.Short.Volumes[i])
+			}
 		}
 		wg.Done()
 	}()
 
 	wg.Wait()
 
-	return maShort.Avg() / maLong.Avg()
+	return maShort.Avg() / maLong.Avg(), nil
 }
--- price/ma_test.go
+++ price/ma_test.go
@@ -11,22 +11,24 @@ import (
 )
 
 func TestMa(t *testing.T) {
-	f, _ := os.OpenFile("./f.csv", os.O_CREATE|os.O_WRONLY, 0755)
+	f, _ := os.OpenFile("./f.csv", os.O_CREATE|os.O_WRONLY|os.O_TRUNC, 0755)
 	defer f.Close()
 
 	w := csv.NewWriter(f)
 
 	count := 200
 	ff := make([]float64, count)
+	vv := make([]float64, count)
 	for i := 0; i < count; i++ {
 		rand.Seed(time.Now().UnixNano())
 		f := rand.Float64() * 10
 		ff[i] = f
-
+		vv[i] = f * 0.01
 		time.Sleep(time.Microsecond)
 	}
 
-	sort.Float64s(ff)
+	sort.Float64s(ff) // 降順
+	// sort.Sort(sort.Reverse(sort.Float64Slice(ff))) // 昇順
 
 	for i := 0; i < count; i++ {
 		w.Write([]string{fmt.Sprintf("%f", ff[i])})
@@ -36,9 +38,14 @@ func TestMa(t *testing.T) {
 	s := &Source{}
 	s.Long.Term = 21
 	s.Long.Prices = ff
+	s.Long.Volumes = vv
 	s.Short.Term = 9
 	s.Short.Prices = ff
+	s.Short.Volumes = vv
 
-	diverd := SMA(s)
-	fmt.Printf("%f\n", diverd)
+	div, err := SMA(true, s)
+	if err != nil {
+		t.Fatal(err)
+	}
+	fmt.Printf("%f\n", div)
 }
