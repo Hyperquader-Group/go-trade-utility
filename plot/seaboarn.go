166ab132ad42e62485ffe7129928459e4afa6318
numbp <o9o73286699@gmail.com>
2020-01-25 19:14:20 +0900

    add subplot, like a seaborn

---
+++ plot/seaboarn.go
@@ -0,0 +1,45 @@
+package plot
+
+import (
+	"github.com/go-gota/gota/dataframe"
+	"github.com/mattn/go-pairplot"
+	"gonum.org/v1/plot"
+	"gonum.org/v1/plot/vg"
+	"strings"
+)
+
+type Seaborn struct {
+	Data dataframe.DataFrame
+}
+
+func NewSeaborn(s interface{}) *Seaborn {
+	var df dataframe.DataFrame
+	switch v := s.(type) {
+	case []string:
+		df = dataframe.ReadCSV(strings.NewReader(strings.Join(v, "\n")))
+	default:
+		df = dataframe.LoadStructs(s)
+	}
+
+	return &Seaborn{
+		Data: df,
+	}
+}
+
+// SubPlot plot like a seaborn, too much consuming
+func (p *Seaborn) SubPlot(w, h int, filename string) error {
+	plt, err := plot.New()
+	if err != nil {
+		return err
+	}
+	pp, err := pairplot.NewPairPlotDataFrame(p.Data)
+	if err != nil {
+		return err
+	}
+	// pp.SetHue("Name")
+	plt.HideAxes()
+	plt.Add(pp)
+	plt.Save(vg.Length(w)*vg.Inch, vg.Length(h)*vg.Inch, filename)
+
+	return nil
+}
