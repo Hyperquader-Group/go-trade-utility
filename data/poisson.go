9d383bab25bdd18ba89cbb4447ee906afaef329e
numbp <o9o73286699@gmail.com>
2020-06-05 00:37:07 +0900

    copy when sort

--- data/poisson.go
+++ data/poisson.go
@@ -66,10 +66,11 @@ func (p *Values) Threshold(isReverse bool, prob float64) float64 {
 		return 0
 	}
 
+	fx := p.values
 	if isReverse {
-		sort.Sort(sort.Reverse(sort.Float64Slice(p.values)))
+		sort.Sort(sort.Reverse(sort.Float64Slice(fx)))
 	} else {
-		sort.Sort(sort.Float64Slice(p.values))
+		sort.Sort(sort.Float64Slice(fx))
 	}
 
 	// mean, stdv := stat.MeanStdDev(p.values, nil)
@@ -77,11 +78,11 @@ func (p *Values) Threshold(isReverse bool, prob float64) float64 {
 	// fmt.Printf("%+v	%v	%v\n", mean, stdv, ex)
 
 	// 累積確率から得たい出現回数の余事象を捨てる
-	point := int(math.Floor(float64(len(p.values)) * prob))
-	if len(p.values) <= point+1 {
+	point := int(math.Floor(float64(len(fx)) * prob))
+	if len(fx) <= point+1 {
 		return 0
 	}
-	return p.values[point+1]
+	return fx[point+1]
 }
 
 func (p *Values) Copy() []float64 {
